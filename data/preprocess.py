43aae4acddfff0e254b0e362de200e2c3e5553c1
Ariel <ariel123good@gmail.com>
2023-11-09 01:39:08 -0800

    complete net ninja youtube course

---
+++ data/preprocess.py
@@ -0,0 +1,21 @@
+import json
+
+words = []
+with open("wordlewords.txt") as f:
+  for line in f:
+    newWords = line.rstrip().split('\t')
+    newWords = list(filter(lambda w: len(w)==5, newWords))
+    words.extend(newWords)
+print(len(words))
+
+data = {}
+data["solutions"] = []
+for i in range(len(words)):
+  data["solutions"].append({"id": i, "word": words[i]})
+
+data["letters"] = []
+for i in range(ord('a'), ord('z')+1):
+  data["letters"].append({"key": chr(i)})
+
+with open('db.json', 'w') as f:
+  json.dump(data, f)
